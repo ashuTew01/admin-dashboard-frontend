f573cfc7e305954b65f5a9eb6785a06ca0af21bc
Ashutosh Tewari <krishwave66@gmail.com>
2024-08-10 20:32:46 +0530

    updates

---
+++ constants.js
@@ -0,0 +1,9 @@
+const textToRoute = {
+	"weekly menu": "menu/weekly",
+	"items & reviews": "menu/items/all",
+	"add a complaint": "complaints/add",
+	latest: "complaints/latest",
+	"top voted": "complaints/top",
+};
+
+export { textToRoute };
